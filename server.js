07f0a9b28b035bcb2a00d0bbb96886f4dba25d69
Shannon <shannon.nell92@gmail.com>
2021-06-21 10:20:07 -0400

    added new port

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 3001;
 //instantiate the server
 const app = express();
 
@@ -52,6 +54,6 @@ app.get('/api/animals', (req, res) => {
 });
 
 //tell it to listen for requests
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
